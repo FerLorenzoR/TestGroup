4bc8c043c07f1de93cbebfa6ade8e9f6cd89df76
Fernando Lorenzo <feer.lorenzoo@hotmail.com>
2023-06-01 12:57:27 -0300

    Mensaje descriptivo

--- client/src/components/views/landingPage/landing.jsx
+++ client/src/components/views/landingPage/landing.jsx
@@ -1,27 +1,21 @@
-import style from './landing.module.css';
-import { Link } from 'react-router-dom'
-
+import style from "./landing.module.css";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
-    
-    return(
-
-        <div className={style.landingContainer} >
-            <div className={style.landingContent} >
-                <h1 className={style.title} > WELCOME TO MY DOG API </h1>
-                <p className={style.p} >
-                    ENTER TO DISCOVER ALL OUR CANINE FRIENDS, 
-                    WHO HAVE SUPPORTED US AS MAN'S BEST FRIEND 
-                    SINCE THE PAST
-                </p>
-                <Link to='/home' >
-                    <button className={style.button} > ENTER </button>
-                </Link>
-            </div>
-        </div>
-
-    );
-
+  return (
+    <div className={style.landingContainer}>
+      <div className={style.landingContent}>
+        <h1 className={style.title}> WELCOME TO MY DOG API </h1>
+        <p className={style.p}>
+          ENTER TO DISCOVER ALL OUR CANINE FRIENDS, WHO HAVE SUPPORTED US AS
+          MAN'S BEST FRIEND SINCE THE PAST
+        </p>
+        <Link to="/home">
+          <button className={style.button}> ENTERATE </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
