dd1eb19459a625f8c21aeb11ed186defbf95346e
Fernando Lorenzo <feer.lorenzoo@hotmail.com>
2023-06-02 12:14:41 -0300

    random

--- client/src/components/views/landingPage/landing.jsx
+++ client/src/components/views/landingPage/landing.jsx
@@ -11,7 +11,7 @@ const Landing = () => {
           MAN'S BEST FRIEND SINCE THE PAST
         </p>
         <Link to="/home">
-          <button className={style.button}> ENTER </button>
+          <button className={style.button}> ENTERs </button>
         </Link>
       </div>
     </div>
